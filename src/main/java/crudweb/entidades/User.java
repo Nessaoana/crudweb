7370110bf18da21e3d899ef4ddfa3217c357d572
Nessaoana <nessaoana09@gmail.com>
2019-08-29 17:51:25 -0300

    Usuário
    Criando classe usuário e pag de cadastro

---
+++ src/main/java/crudweb/entidades/User.java
@@ -0,0 +1,41 @@
+package crudweb.entidades;
+
+public class User {
+	private String nome;
+	private int idade;
+	private String sexo;
+	
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getSexo() {
+		return this.sexo;
+	}
+	
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	
+	User(String nome, int idade){
+		this.nome = nome;
+		this.idade = idade;
+	}
+	
+	
+}
